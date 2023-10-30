7ca7cd7031190f91dd8c19f029b3f633734f1f41
Jess Atkinson <jesslynn1007@gmail.com>
2023-10-30 16:57:48 -0400

    end of monday

---
+++ frontend/frontend/src/app/app.routes.ts
@@ -0,0 +1,26 @@
+import { Routes } from "@angular/router";
+import { HomeComponent } from "./componenets/home/home.component";
+import { TodosComponent } from "./pages/todos.component";
+import { CounterComponent } from "./pages/counter.component";
+ 
+export const routes: Routes = [
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+
+    {
+        path: "todos",
+        component: TodosComponent
+    },
+
+    {
+        path: "counter",
+        component: CounterComponent
+    },
+
+    {
+        path: '**',
+        redirectTo: 'home'
+      }
+]
---
+++ frontend/frontend/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  daisyui: {
+    themes:["valentine"]
+  },
+  theme: { 
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('daisyui')
+  ],
+}
+
